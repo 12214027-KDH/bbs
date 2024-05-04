93c51816baf0bb491d679df473e88f8f2ea93720
DongHan Kim <kd_h@naver.com>
2024-05-04 12:37:16 +0900

    work9 commit

---
+++ src/firebaseInit.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7vfUscXmvPj121DC77rWt3rA6nbTjdlk",
+  authDomain: "inha-58e5d.firebaseapp.com",
+  projectId: "inha-58e5d",
+  storageBucket: "inha-58e5d.appspot.com",
+  messagingSenderId: "654471481212",
+  appId: "1:654471481212:web:873cecea178810e5eac6f9"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
